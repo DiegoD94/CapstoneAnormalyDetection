4f2372a4c3e188b7c29eb6dde4cffee3e7661193
zhuzilin <zhuzilinallen@gmail.com>
2019-10-13 17:20:34 -0400

    add readme

--- data_generated_library/test.py
+++ data_generated_library/test.py
@@ -108,3 +108,55 @@ plt.figure()
 ax = plt.gca()
 ax.scatter(x, y)
 plt.savefig("example/f5.png")
+
+"""
+figure 6
+AWS
+"""
+
+t0 = 0
+t1 = 20000
+t2 = 25000
+t3 = 35000
+t4 = 40000
+t5 = 60000
+
+def trend_func(t):
+    if t < t1:
+        return 48
+    elif t < t2:
+        return 48 + (45-48)*(t - t1)/(t2 - t1)
+    elif t < t3:
+        return 45
+    elif t < t4:
+        return 45 + (40-45)*(t - t3)/(t4 - t3)
+    else:
+        return 40
+
+def std_func(t):
+    if t < t1:
+        return 3
+    elif t < t2:
+        return 3 + (2.5-3)*(t - t1)/(t2 - t1)
+    elif t < t3:
+        return 2.5
+    elif t < t4:
+        return 2.5 + (1-2.5)*(t - t3)/(t4 - t3)
+    else:
+        return 1
+
+gauss = StatFunc(random.gauss, {"mu": 0, "sigma": std_func})
+
+ts = TimeSeries([gauss], [1], trend_func=trend_func)
+
+x = []
+y = []
+for t in range(t5):
+    t, v, color = ts.next()
+    x.append(t)
+    y.append(v)
+
+plt.figure()
+ax = plt.gca()
+ax.plot(x, y)
+plt.savefig("example/aws.png")
