ad04bdd2cb5d0d83ce9ef16958cb0a93a7e9955b
zhuzilin <zhuzilinallen@gmail.com>
2019-11-21 02:27:34 -0500

    add hmm predict baseline

--- data_generated_library/hmm.py
+++ data_generated_library/hmm.py
@@ -2,6 +2,10 @@ from time_series import HMM
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from hmmlearn.hmm import MultinomialHMM
+from functools import reduce
+
+
+np.set_printoptions(precision=2)
 
 GOOD = 0
 STRUGGLE = 1
@@ -49,8 +53,59 @@ for _ in range(N):  #  number of customer
     m_Y.append(Y)
 
 ####################################################
+# hmm predict
+####################################################
+print("--------------------------------------------")
+print("HMM:")
+def preprocess_data_hmm(Y):
+    data = np.concatenate([np.reshape(y, (-1, 1)) for y in Y])
+    lengths = [len(y) for y in Y]
+    return data, lengths
+train_data, train_lengths = preprocess_data_hmm(m_Y[:int(0.8*N)])
+test_data, test_lengths = preprocess_data_hmm(m_Y[int(0.8*N):])
+test_label = reduce(lambda x, y: x + y, m_Z[int(0.8*N):])
+
+hmm = MultinomialHMM(n_components=3, n_iter=50)
+hmm.fit(train_data, test_lengths)
+print("hidden state transition matrix:")
+print(hmm.transmat_)
+print("observation matrix:")
+print(hmm.emissionprob_)
+# print(m_Z[0])
+tmp_pred = hmm.predict(np.reshape(m_Z[0], (-1, 1)))
+# print(tmp_pred)
+mdict = {}
+mdict[tmp_pred[0]] = 0
+mdict[tmp_pred[-1]] = 2
+mdict[3 - tmp_pred[0] - tmp_pred[-1]] = 1
+vfunc = np.vectorize(mdict.get)
+# print(mdict)
+# print(vfunc(tmp_pred))
+predict = vfunc(hmm.predict(test_data, test_lengths))
+TP, TN, FP, FN = 0, 0, 0, 0
+for i in range(len(predict)):
+    if i > 0 and test_label[i-1] == 2 and test_label[i] == 2:
+        continue
+    if test_label[i] == 2 and predict[i] == 2:
+        TP += 1
+    if test_label[i] != 2 and predict[i] == 2:
+        FP += 1
+    if test_label[i] == 2 and predict[i] != 2:
+        FN += 1
+    if test_label[i] != 2 and predict[i] != 2:
+        TN += 1
+print("accuracy: ")
+print((TP + TN) / (TP + TN + FP + FN))
+print("precision: ")
+print(TP / (TP + FP))
+print("recall: ")
+print((TP) / (TP + FN))
+
+####################################################
 # logistic regression
 ####################################################
+print("--------------------------------------------")
+print("LR:")
 # create windows data
 win_size = 5
 train_data = []
@@ -91,7 +146,7 @@ for i in range(len(test_data)):
 
 print("accuracy: ")
 print(lr.score(test_data, test_label))
-print("TP: ")
+print("recall: ")
 print(lr.score(test_neg_data, test_neg_label))
 
 for i in range(5):
