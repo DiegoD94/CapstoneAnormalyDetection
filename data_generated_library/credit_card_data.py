b038f12ad75dc90fc939d1a8b2ba4c8571f669b6
zhuzilin <zhuzilinallen@gmail.com>
2019-11-23 16:05:52 -0500

    seperate model and data generation for credit card data

---
+++ data_generated_library/credit_card_data.py
@@ -0,0 +1,115 @@
+import numpy as np
+from time_series import HMM
+
+np.set_printoptions(precision=2)
+
+GOOD = 0
+STRUGGLE = 1
+BAD = 2
+
+ALL = 0
+INTEREST = 1
+NOTHING = 2
+
+# standard matrix
+# good, struggle, bad
+m_hidden = [[0.89, 0.1, 0.01],  # good
+            [0.1, 0.7, 0.2],  # struggle
+            [0, 0, 1]  # bad
+           ]
+
+# pay all, pay interest, pay nothing
+m_observe = [[0.99, 0.01, 0],  # good
+             [0.5, 0.5, 0],  # struggle
+             [0, 0.2, 0.8]  # bad
+            ]
+
+def generate_m(dists):
+    assert len(dists) == 9
+    m = np.zeros((3, 3))
+    for i in range(3):
+        for j in range(3):
+            m[i][j] = dists[3*i+j]()
+    for i in range(3):
+        m[i] /= m[i].sum()
+    return m
+
+def generate_m_hidden():
+    dists = []
+    dists.append(lambda: np.random.uniform(0.79, 0.99))
+    dists.append(lambda: np.random.uniform(0.05, 0.15))
+    dists.append(lambda: np.random.uniform(0.01, 0.02))
+
+    dists.append(lambda: np.random.uniform(0.05, 0.15))
+    dists.append(lambda: np.random.uniform(0.65, 0.75))
+    dists.append(lambda: np.random.uniform(0.15, 0.25))
+
+    dists.append(lambda: 0)
+    dists.append(lambda: 0)
+    dists.append(lambda: 1)
+    return generate_m(dists)
+
+def generate_m_observe():
+    dists = []
+    dists.append(lambda: np.random.uniform(0.95, 0.99))
+    dists.append(lambda: np.random.uniform(0.01, 0.02))
+    dists.append(lambda: 0)
+
+    dists.append(lambda: np.random.uniform(0.45, 0.55))
+    dists.append(lambda: np.random.uniform(0.45, 0.55))
+    dists.append(lambda: 0)
+
+    dists.append(lambda: 0)
+    dists.append(lambda: np.random.uniform(0.15, 0.25))
+    dists.append(lambda: np.random.uniform(0.75, 0.85))
+    return generate_m(dists)
+
+def generate_raw_data(T, N):
+    m_Z = []
+    m_Y = []
+    for _ in range(N):  #  number of customer
+        Z = []
+        Y = []
+        m_hidden = generate_m_hidden()
+        m_observe = generate_m_observe()
+        hmm = HMM(GOOD, m_hidden, m_observe)
+        for _ in range(T):  #  time stamp
+            z, y = hmm.next()
+            Z.append(z)
+            Y.append(y)
+        # print
+        m_Z.append(Z)
+        m_Y.append(Y)
+    return m_Z, m_Y
+
+def generate_win_data(m_Z, m_Y, win_size):
+    assert(len(m_Z) > 0)
+    N = len(m_Z)
+    T = len(m_Z[0])
+
+    train_data = []
+    train_label = []
+    for i in range(int(0.8*N)):
+        for t in range(T-win_size):
+            if m_Z[i][t+win_size-1] == 2:
+                break
+            train_data.append(m_Y[i][t:t+win_size])
+            if m_Z[i][t+win_size] == 2:
+                train_label.append(1)
+                break
+            else:
+                train_label.append(0)
+
+    test_data = []
+    test_label = []
+    for i in range(int(0.8*N), N):
+        for t in range(T-win_size):
+            if m_Z[i][t+win_size-1] == 2:
+                break
+            test_data.append(m_Y[i][t:t+win_size])
+            if m_Z[i][t+win_size] == 2:
+                test_label.append(1)
+                break
+            else:
+                test_label.append(0)
+    return train_data, train_label, test_data, test_label
--- data_generated_library/hmm.py
+++ data_generated_library/hmm.py
@@ -1,62 +1,17 @@
-from time_series import HMM
-import numpy as np
+from credit_card_data import generate_raw_data, generate_win_data
 from sklearn.linear_model import LogisticRegression
 from hmmlearn.hmm import MultinomialHMM
 from functools import reduce
-
-
-np.set_printoptions(precision=2)
-
-GOOD = 0
-STRUGGLE = 1
-BAD = 2
-
-ALL = 0
-INTEREST = 1
-NOTHING = 2
-
-# good, struggle, bad
-m_hidden = [[0.89, 0.1, 0.01],  # good
-            [0.1, 0.7, 0.2],  # struggle
-            [0, 0, 1]  # bad
-           ]
-
-# pay all, pay interest, pay nothing
-m_observe = [[0.99, 0.01, 0],  # good
-             [0.5, 0.5, 0],  # struggle
-             [0, 0.2, 0.8]  # bad
-            ]
-
-T = 60
-N = 10000
-m_Z = []
-m_Y = []
-for _ in range(N):  #  number of customer
-    Z = []
-    Y = []
-    hmm = HMM(GOOD, m_hidden, m_observe)
-    for _ in range(T):  #  time stamp
-        z, y = hmm.next()
-        Z.append(z)
-        Y.append(y)
-    # print
-    """
-    for t in range(T):
-        print(Z[t], end="")
-    print()
-    for t in range(T):
-        print(Y[t], end="")
-    print()
-    print()
-    """
-    m_Z.append(Z)
-    m_Y.append(Y)
+import numpy as np
 
 ####################################################
 # hmm predict
 ####################################################
 print("--------------------------------------------")
 print("HMM:")
+T = 60
+N = 100
+m_Z, m_Y = generate_raw_data(T, N)
 def preprocess_data_hmm(Y):
     data = np.concatenate([np.reshape(y, (-1, 1)) for y in Y])
     lengths = [len(y) for y in Y]
@@ -108,32 +63,7 @@ print("--------------------------------------------")
 print("LR:")
 # create windows data
 win_size = 5
-train_data = []
-train_label = []
-for i in range(int(0.8*N)):
-    for t in range(T-win_size):
-        if m_Z[i][t+win_size-1] == 2:
-            break
-        train_data.append(m_Y[i][t:t+win_size])
-        if m_Z[i][t+win_size] == 2:
-            train_label.append(1)
-            break
-        else:
-            train_label.append(0)
-
-test_data = []
-test_label = []
-for i in range(int(0.8*N), N):
-    for t in range(T-win_size):
-        if m_Z[i][t+win_size-1] == 2:
-            break
-        test_data.append(m_Y[i][t:t+win_size])
-        if m_Z[i][t+win_size] == 2:
-            test_label.append(1)
-            break
-        else:
-            test_label.append(0)
-
+train_data, train_label, test_data, test_label = generate_win_data(m_Z, m_Y, win_size)
 lr = LogisticRegression(class_weight="balanced")
 lr.fit(train_data, train_label)
 
